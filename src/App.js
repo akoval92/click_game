da8f6ebaf6fccbc6270c80b5cb0de5f995227964
Amber Koval <amberkoval92@gmail.com>
2019-07-12 17:46:15 -0700

    fixed react repo

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import images from "./images.json";
+
+
+function App() {
+  console.log(images)
+  return <p>YO!</p>;
+}
+
+export default App;
---
+++ src/components/clicky.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+
+
+function HelloReact() {
+  return <p>Hello World!</p>;
+}
+
+export default HelloReact;
